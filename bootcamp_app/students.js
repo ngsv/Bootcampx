5466c3d58262368143d0905017e2d8e85a9efb3f
Steven Ngov <gl.steven.ngov@gmail.com>
2022-01-19 02:56:40 +0000

    Update students.js and teachers.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,6 +9,9 @@ const pool = new Pool({
 
 // Command-line arguments
 const cmdArgs = process.argv.slice(2);
+const cohortName = cmdArgs[0];
+const limit = cmdArgs[1] || 5;
+const values = [`%${cohortName}%`, limit];
 
 pool.query(`
   SELECT id, name, cohort_id
@@ -25,9 +28,9 @@ pool.query(`
   SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort
   FROM students
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${cmdArgs[0]}%'
-  LIMIT ${cmdArgs[1] || 5};
-  `)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,6 +9,8 @@ const pool = new Pool({
 
 // Command-line arguments
 const cmdArgs = process.argv.slice(2);
+const cohortName = cmdArgs[0] || 'JUL02';
+const values = [cohortName];
 
 pool.query(`
   SELECT teachers.name AS teacher, cohorts.name AS cohort, COUNT(assistance_requests.*) AS total_assistances
@@ -16,10 +18,10 @@ pool.query(`
   JOIN assistance_requests ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = '${cmdArgs[0] || 'JUL02'}'
+  WHERE cohorts.name = $1
   GROUP BY teachers.name, cohorts.name
   ORDER BY teachers.name;
-  `)
+  `, values)
   .then(res => {
     res.rows.forEach(teacher => {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
